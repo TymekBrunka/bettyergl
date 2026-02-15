6baefa29dd66b05f5369b47c570db423cc73a142
TymekBrunka <tymkopolo@wp.pl>
2026-02-15 18:11:22 +0100

    added ccache support - wow that's fast

--- deps/ext.cmake
+++ deps/ext.cmake
@@ -1,8 +1,19 @@
 include(deps/CPM.cmake)
 
+find_program(CCACHE_PROGRAM ccache)
+if (CCACHE_PROGRAM)
+  message(found)
+  set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE_PROGRAM} base_dir=${PROJECT_SOURCE_DIR} hash_dir=false)
+  set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_PROGRAM} base_dir=${PROJECT_SOURCE_DIR} hash_dir=false)
+endif()
+
 CPMAddPackage(
   NAME glfw
-  VERSION 3.0
-  GITHUB_REPOSITORY "glfw/glfw"
+  VERSION 3.4
+  GITHUB_REPOSITORY glfw/glfw
   GIT_TAG 3.4
+  OPTIONS
+    "GLFW_BUILD_EXAMPLES OFF"
+    "GLFW_BUILD_TESTS OFF"
+    "GLFW_BUILD_DOCS OFF"
 )
