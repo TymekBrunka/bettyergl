2a976c6325a5a141badf87796c2527bc44f1f97d
TymekBrunka <tymkopolo@wp.pl>
2026-03-19 08:37:30 +0100

    changed math types from c arrays to assignable std::array's

--- src/include/bettergl/MathTypes.hpp
+++ src/include/bettergl/MathTypes.hpp
@@ -1,11 +1,12 @@
 #pragma once
+#include <array>
 namespace bgl {
 
-typedef float vec2[2];
-typedef float vec3[3];
-typedef float vec4[4];
-typedef float mat2[2];
-typedef float mat3[3];
-typedef float mat4[4];
+typedef std::array<float, 2> vec2;
+typedef std::array<float, 3> vec3;
+typedef std::array<float, 4> vec4;
+typedef std::array<float, 4> mat2;
+typedef std::array<float, 9> mat3;
+typedef std::array<float, 16> mat4;
 
 } // namespace bgl
